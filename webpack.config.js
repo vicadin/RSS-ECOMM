2e159c44004acbfa3a75bd8b72fd88888d4bb9ac
victoria tonkovich <tonkovich.vika@gmail.com>
2024-05-04 14:28:34 +0300

    docs: add TypeScript configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,46 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: {
-    main: path.resolve(__dirname, "./script.js"),
-  },
+  mode: "development",
+  entry: "./src/main.ts",
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "script.bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+    new MiniCssExtractPlugin(),
+    /*    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/assets"),
+          to: path.resolve(__dirname, "dist/assets"),
+        },
+      ],
+    }), */
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+      },
+    ],
   },
 };
