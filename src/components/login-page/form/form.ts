9563e0898d7b3408cb8e860dbf817901cbf090bf
pirnataly <piroznatal@gmail.com>
2024-05-10 20:03:50 +0300

    feat: add email field

--- src/components/login-page/form/form.ts
+++ src/components/login-page/form/form.ts
@@ -3,13 +3,76 @@ import "./form.css";
 export class Form {
   form: HTMLFormElement;
 
+  emailWrapper: HTMLDivElement;
+
+  emailInput: HTMLInputElement;
+
+  emailInnerWrapper: HTMLDivElement;
+
+  emailLabel: HTMLLabelElement;
+
+  emailError: HTMLSpanElement;
+
   constructor() {
     this.form = document.createElement("form");
+    this.form.setAttribute("novalidate", "novalidate");
     this.form.className = "form";
+    this.emailWrapper = document.createElement("div");
+    this.emailWrapper.className = "email-wrapper";
+    this.emailInnerWrapper = document.createElement("div");
+    this.emailInput = document.createElement("input");
+    this.emailInput.className = "email-input";
+    this.emailInput.setAttribute("type", "email");
+    this.emailInput.setAttribute("required", "required");
+    this.emailInput.setAttribute("autocomplete", "off");
+    this.emailInput.setAttribute("pattern", "[a-z0-9._%+\\-]+@[a-z0-9.\\-]+\\.[a-z]{2,}$");
+    this.emailLabel = document.createElement("label");
+    this.emailLabel.className = "email-label";
+    this.emailLabel.textContent = "Email address";
+    this.emailInnerWrapper.append(this.emailInput, this.emailLabel);
+    this.emailInnerWrapper.className = "email-inner-wrapper";
+    this.emailError = document.createElement("span");
+    this.emailError.className = "email-error";
+    this.emailWrapper.append(this.emailInnerWrapper, this.emailError);
+    this.form.append(this.emailWrapper);
+    this.addEventListeners();
   }
 
   getHtmlElem() {
     return this.form;
+  }
+
+  addEventListeners() {
+    this.emailInput.addEventListener("focusin", () => {
+      this.emailLabel.classList.add("email-label_moved");
+    });
+
+    this.emailInput.addEventListener("focusout", () => {
+      if (this.emailInput.value === "") {
+        this.emailLabel.classList.remove("email-label_moved");
+      }
+    });
+
+    this.form.addEventListener("submit", (ev) => {
+      ev.preventDefault();
+      if (this.emailInput.validity.valid) {
+        // запрос после проверки
+      } else {
+        if (this.emailInput.validity.valueMissing) {
+          this.emailError.textContent = "This field is required";
+        }
+        this.emailError.classList.add("email-error_active");
+      }
+    });
+
+    this.emailInput.addEventListener("input", () => {
+      if (!this.emailInput.validity.valid) {
+        this.emailError.textContent =
+          "Please enter an email address in the correct format, such as name@example.com";
+      } else {
+        this.emailError.textContent = "";
+      }
+    });
   }
 }
 
--- src/components/login-page/login-page.ts
+++ src/components/login-page/login-page.ts
@@ -9,16 +9,16 @@ export default class LoginPage {
     this.loginPage.className = "login-page";
     const loginMainContainer = document.createElement("div");
     loginMainContainer.className = "login-main-container";
+    const loginWrapper = document.createElement("div");
+
     const loginHeading = document.createElement("h1");
     loginHeading.className = "login-heading";
     loginHeading.textContent = "Log in to your account";
-    loginMainContainer.append(loginHeading);
+
+    loginWrapper.append(loginHeading);
+    loginMainContainer.append(loginWrapper);
     loginMainContainer.append(form.getHtmlElem());
     this.loginPage.append(loginMainContainer);
-  }
-
-  render(parent: HTMLElement) {
-    parent.append(this.loginPage);
   }
 
   getHtmlElem() {
